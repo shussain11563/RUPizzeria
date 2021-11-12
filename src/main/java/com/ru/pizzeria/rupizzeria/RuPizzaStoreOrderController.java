00ab90a0b36983a4f6a3c0a6d4169bb275e0c36c
shussain11563 <44513446+shussain11563@users.noreply.github.com>
2021-11-12 15:44:06 -0500

    Added method that will clear view after order submission

--- src/main/java/com/ru/pizzeria/rupizzeria/RuPizzaStoreOrderController.java
+++ src/main/java/com/ru/pizzeria/rupizzeria/RuPizzaStoreOrderController.java
@@ -1,27 +1,84 @@
 package com.ru.pizzeria.rupizzeria;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class RuPizzaStoreOrderController {
-    @FXML
-    private Button cancelOrderButton;
 
-    @FXML
-    private Button exportStoreOrdersButton;
+public class RuPizzaStoreOrderController implements Initializable {
 
     @FXML
-    private ComboBox<String> customerPhoneNumberComboBox;
+    private TextArea customerPhoneNumber;
+
+    @FXML
+    private ListView<Pizza> orderListView;
 
     @FXML
     private TextArea orderTotalTextArea;
 
     @FXML
-    private ListView<String> storeOrderListView;
+    private Button placeOrderButton;
+
+    @FXML
+    private Button removeSelectedPizzaButton;
+
+    @FXML
+    private TextArea salesTaxTextArea;
+
+    @FXML
+    private TextArea subtotalTextArea;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+
+        //subtotal
+        //sales tax
+        //order total
+
+        //might requrie
+        //phone number
+        //order
+
+
+    }
+
+    //when add to order
+    private void clear()
+    {
+        //make a private method that forced textArea to be uneditable
+        this.salesTaxTextArea.clear();
+        this.subtotalTextArea.clear();
+        this.orderTotalTextArea.clear();
+        this.customerPhoneNumber.clear();
+    }
+
+
+
+    @FXML
+    void placeOrder(ActionEvent event)
+    {
+
+
+        clear();
+
+    }
+
+    @FXML
+    void removeSelectedPizza(ActionEvent event)
+    {
+
+    }
+
+
+
 
 }
 
